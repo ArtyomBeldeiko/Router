4f92313bf7305f3a22502034052fb301e2670a49
ArtyomBeldeiko <artyom.beldeiko@gmail.com>
2022-08-07 00:08:50 +0300

    Initial Commit

---
+++ Router/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Router
+//
+//  Created by Artyom Beldeiko on 7.08.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
